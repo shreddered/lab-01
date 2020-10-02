3c101f553118ecf0a3f865f73ea287470eb4b116
Dmitry Kalinin <mitya17092001@gmail.com>
2020-10-02 18:38:11 +0300

    got rid of unused modules

--- source/app.d
+++ source/app.d
@@ -1,9 +1,6 @@
 import algorithms;
 
-import std.algorithm : map, min, minIndex;
-import std.array : array;
 import std.math : exp;
-import std.range : iota;
 import std.stdio : writeln, writefln;
 
 alias fun = (x) => (1 - x) * (1 - x) + exp(x);
