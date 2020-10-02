5dcc17c28b09df2db28cec153b711fa85c8930a6
Dmitry Kalinin <mitya17092001@gmail.com>
2020-10-02 18:18:56 +0300

    added demo of fibonacci search

--- source/app.d
+++ source/app.d
@@ -10,8 +10,12 @@ alias fun = (x) => (1 - x) * (1 - x) + exp(x);
 
 int main() {
     // optimalPassiveSearch!fun(-5, 2);
-    auto searcher = new OptimalPassiveSearcher!fun;
+    ISearcher searcher = new OptimalPassiveSearcher!fun;
     searcher.setInterval(-5, 2);
-    searcher.search;
+    searcher.search(0.1);
+
+    searcher = new FibonacciSearcher!fun;
+    searcher.setInterval(-5, 2);
+    searcher.search(0.1);
     return 0;
 }
